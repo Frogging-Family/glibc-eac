2d3cea11075059bed6aeddc4957acc88fa97b2d9
Etienne JUVIGNY <ti3nou@gmail.com>
2024-06-05 17:42:07 +0200

    Added _upstream_commit variable to set a user defined glibc commit in case of regression

--- glibc_eac.sh
+++ glibc_eac.sh
@@ -2,6 +2,7 @@
   _reenable_dt_hash="true"
   _rogue_company_fix="true"
   _disable_tests="true" # They are very playful and seem to fail randomly in different ways depending on the machine, so let's disable them by default - https://github.com/Frogging-Family/glibc-eac/issues/2
+  _upstream_commit="" # 2.39-4 commit was 31da30f23cddd36db29d5b6a1c7619361b271fb4
 
   rm -rf ./glibc && git clone --depth=1 --single-branch -b main https://gitlab.archlinux.org/archlinux/packaging/packages/glibc.git
   cd ./glibc
@@ -16,6 +17,10 @@
 
   if [ "$_disable_tests" = "true" ]; then
     patch -Np1 -i ../disable_tests.patch
+  fi
+
+  if [ -n "$_upstream_commit" ]; then
+    sed -i "s/_commit=.*/_commit=$_upstream_commit/g" PKGBUILD
   fi
 
   if [ "$1" = "build" ]; then
