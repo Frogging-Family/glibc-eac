b9a3da4f56ce74c7cfdc6524f9ea82a921e7e4bc
xiota <github@mentalfossa.com>
2024-09-08 18:20:13 +0000

    Change method of injecting fixes Make variables easier to change Add status messages

--- glibc_eac.sh
+++ glibc_eac.sh
@@ -1,27 +1,55 @@
 #!/usr/bin/env bash
 
 # Enable/disable each fix we offer
-_reenable_dt_hash="true"
-_rogue_company_fix="false"
-_disable_tests="true" # They are very playful and seem to fail randomly in different ways depending on the machine, so let's disable them by default - https://github.com/Frogging-Family/glibc-eac/issues/2
-_upstream_commit=""   # 2.39-4 commit was 31da30f23cddd36db29d5b6a1c7619361b271fb4
+: ${_reenable_dt_hash=true}
+: ${_rogue_company_fix=false}
+: ${_disable_tests=true} # random fails on different machines
+: ${_upstream_commit=}   # 2.40+r16+gaa533d58ff = aa533d58ff12e27771d9c960a727d74992a3f2a3
 
 rm -rf ./glibc && git clone --depth=1 --single-branch -b main https://gitlab.archlinux.org/archlinux/packaging/packages/glibc.git
 cd ./glibc
 
-if [ "$_reenable_dt_hash" = "true" ]; then
-  patch -Np1 -i ../inject_reenable_DT_HASH.patch && cp ../reenable_DT_HASH.patch ./
+if [[ "${_reenable_dt_hash::1}" =~ t|y|1 ]]; then
+  echo "::: Fixing DT_HASH..."
+  cat >> PKGBUILD << 'END'
+export LDFLAGS+=" -Wl,--hash-style=both"
+END
 fi
 
-if [ "$_rogue_company_fix" = "true" ]; then
-  patch -Np1 -i ../inject_rogue_company_reverts.patch && cp ../rogue_company_reverts.patch ./
+if [[ "${_rogue_company_fix::1}" =~ t|y|1 ]]; then
+  echo "::: Fixing Rogue Company..."
+  cp ../rogue_company_reverts.patch ./
+  cat >> PKGBUILD << 'END'
+source+=('rogue_company_reverts.patch')
+b2sums+=('SKIP')
+
+eval _orig_"$(declare -f prepare)"
+prepare() {
+  (_orig_prepare)
+
+  cd glibc
+  # Reverts for Rogue Company to work again
+  # 7a5db2e82fbb6c3a6e3fdae02b7166c5d0e8c7a8
+  # 8208be389bce84be0e1c35a3daa0c3467418f921
+  # 6bf789d69e6be48419094ca98f064e00297a27d5
+  # b89d5de2508215ef3131db7bed76ac50b3f4c205
+  # 86f0179bc003ffc34ffaa8d528a7a90153ac06c6
+  patch -Np1 -F100 -i "../rogue_company_reverts.patch"
+}
+END
 fi
 
-if [ "$_disable_tests" = "true" ]; then
-  patch -Np1 -i ../disable_tests.patch
+# https://github.com/Frogging-Family/glibc-eac/issues/2
+if [[ "${_disable_tests::1}" =~ t|y|1 ]]; then
+  echo "::: Disabling unit tests..."
+  cat >> PKGBUILD << 'END'
+unset checkdepends
+unset -f check
+END
 fi
 
 if [ -n "$_upstream_commit" ]; then
+  echo -n "::: Using commit: $_upstream_commit"
   sed -i "s/_commit=.*/_commit=$_upstream_commit/g" PKGBUILD
 fi
 
@@ -29,7 +57,7 @@ if [ "$1" = "build" ]; then
   makepkg --noconfirm --skipinteg -sc
 else
   echo "############################################################################################"
-  echo "! Defaulting to building then installing ! If you only want to build, run './glibc_eac.sh build'"
+  echo "!! Default is to build and install !! If you want to build only, run: './glibc_eac.sh build'"
   echo "############################################################################################"
   read -rp "Press enter to continue or hit ctrl+c to leave"
   makepkg --noconfirm --skipinteg -sic
